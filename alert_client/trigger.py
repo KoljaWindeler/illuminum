e1425088e5f07384d18df9102b61ee222955a047
Kolja RPI <kkoolljjaa@gmail.com>
2015-04-24 12:29:00 -0500

    resolution change

--- alert_client/trigger.py
+++ alert_client/trigger.py
@@ -11,11 +11,11 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-#WIDTH=1280
-#HEIGHT=720
-WIDTH=640
-HEIGHT=480
-TIMING_DEBUG=0
+WIDTH=1280
+HEIGHT=720
+#WIDTH=640
+#HEIGHT=480
+TIMING_DEBUG=1
 
 
 
@@ -25,8 +25,12 @@ def start():
 #******************************************************#
 def start_trigger():
 	while True:
+		global cam_width
+		global cam_heigh
+		global change_res_event
+	
 		try:
-			print("[A "+time.strftime("%H:%M:%S")+"] -> Starting Camera Interface")
+			print("[A "+time.strftime("%H:%M:%S")+"] -> Starting Camera Interface ("+str(cam_width)+"/"+str(cam_height)+")")
 			os.system('v4l2-ctl -d 0 -c focus_auto=1')
 			os.system('v4l2-ctl -d 0 -c contrast=1')
 			os.system('v4l2-ctl -d 0 -c saturation=83')
@@ -34,7 +38,7 @@ def start_trigger():
 			os.system('v4l2-ctl -d 0 -c brightness=130')
 			os.system('v4l2-ctl -d 0 -c white_balance_temperature_auto=1')
 			pygame.camera.init()
-			cam = pygame.camera.Camera("/dev/video0",(WIDTH,HEIGHT))
+			cam = pygame.camera.Camera("/dev/video0",(cam_width,cam_height))
 			cam.start()
 		except:
 			print("Could not start the Camera!")
@@ -42,6 +46,10 @@ def start_trigger():
 		#setup GPIO using Board numbering
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+		#font
+		f=ImageFont.load_default()
+
 
 		global last_webcam_ts
 		global webcam_interval
@@ -64,6 +72,13 @@ def start_trigger():
 				print("Trouble reading GPIO, restarting")
 				break
 
+			if(change_res_event==1):
+				print("changing resolution ("+str(cam_width)+"/"+str(cam_height)+")")
+				change_res_event=0
+				cam.stop()
+				cam=pygame.camera.Camera("/dev/video0",(cam_width,cam_height))
+				cam.start()
+				
 			if(gpio_state != state and detection==1):
 				busy=1
 				if(gpio_state == 1):
@@ -123,10 +138,9 @@ def start_trigger():
 					pic_t = time.time()
 			
 				pil_string_image = pygame.image.tostring(img, "RGBA",False)
-				img = Image.fromstring("RGBA",(WIDTH,HEIGHT),pil_string_image)
+				img = Image.fromstring("RGBA",(cam_width,cam_height),pil_string_image)
 				
 				draw=ImageDraw.Draw(img)
-				f=ImageFont.load_default()
 				draw.text((0, 0),path+" "+time.strftime("%H:%M:%S"),(10,10,10),font=f)
 
 				if(TIMING_DEBUG):
@@ -166,9 +180,27 @@ def set_detection(state):
 def set_interval(interval):
 	global webcam_interval
 	webcam_interval=interval
+	if(interval>0):
+		change_res((1280,720))
+		#change_res((640,480))
+	else:
+		change_res((1280,720))
 	
+def change_res(res):
+	global change_res_event
+	global cam_width
+	global cam_height
+	if(cam_width!=res[0] or cam_height!=res[1]):
+		change_res_event=1
+		cam_width=res[0]
+		cam_height=res[1]
 
+
+##########################################
 webcam_interval=0
+change_res_event=0
+cam_width=WIDTH
+cam_height=HEIGHT
 last_webcam_ts=time.time()
 callback_action=[subscribe_callback]
 detection=0 # set to 1 for default
