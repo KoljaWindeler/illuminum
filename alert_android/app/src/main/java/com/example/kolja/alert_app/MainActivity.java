6dc5b143fc3d476136fca325518aeb8748ab5d73
KoljaWindeler <Kolja.Windeler@gmail.com>
2015-04-30 13:08:31 -0500

    wrong scr
    fixed src

--- alert_android/app/src/main/java/com/example/kolja/alert_app/MainActivity.java
+++ alert_android/app/src/main/java/com/example/kolja/alert_app/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Build;
@@ -20,10 +19,8 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.net.URI;
 import java.net.URISyntaxException;
-
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
@@ -95,8 +92,51 @@ public class MainActivity extends ActionBarActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        //TextView textView = (TextView)findViewById(R.id.messages);
-                        //textView.setText(textView.getText() + "\n" + message);
+                        TextView textView = (TextView)findViewById(R.id.messages);
+                        textView.setText(textView.getText() + "\n" + message);
+						
+			JSONObject object = new JSONObject();
+			object.fromString(message);
+			try {
+				cmd=object.getString("cmd");
+			} catch (e) {
+				cmd=""
+			}
+			
+			if(cmd=="rf"){					
+				try {
+					String encodedImage=object.getString("data");
+					Log.i("websocket","got str from obj");
+					byte[] decodedString = Base64.decode(encodedImage, Base64.URL_SAFE);
+					Log.i("websocket","decoded");
+					Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+					Log.i("websocket","loaded bitmap");
+					(Image)findViewById(R.id.img).setImageBitmap(decodedByte);
+					Log.i("websocket","bitmap set");
+				}	catch (e) {
+					int ignore=0;
+				}
+			}
+			
+			else if(cmd=="m2v_login"){
+				try {
+					// add an object that will hold the ID "mid", "area","state","account"
+					String mid=object.getString("mid");
+					String area=object.getString("area");
+					int state=object.getString("state");
+					String account=object.getString("account");
+					
+					// quick and dirty: add us to the list of webcams as soon as they sign up:
+					JSONObject object_send = new JSONObject();
+					object_send.put("cmd", "set_interval");
+					object_send.put("mid", mid);
+					object_send.put("interval", "3");
+					mWebSocketClient.send(object_send.toString());
+				}	catch (e) {
+					int ignore=0;
+				}
+			}
+						
                     }
                 });
             }
