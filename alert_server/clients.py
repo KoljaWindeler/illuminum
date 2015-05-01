260565c3a7195fe42425d3086a13ed46a5d46a22
Kolja banana <kkoolljjaa@gmail.com>
2015-04-30 21:37:03 -0500

    added state strings

--- alert_server/clients.py
+++ alert_server/clients.py
@@ -1,4 +1,7 @@
 #A info container for each camera client
+m2m_state = ["idle","alert","disabled,idle","disabled,movement","error"]
+
+
 class m2m_clients: 
 	def __init__(self, addr):
 		self.ip = addr.getpeername()[0]  	# the IP adress
@@ -15,10 +18,10 @@ class m2m_clients:
 		self.m2v=[]				# list of all active viewer subscribers. those will get a message for e.g. idle -> motion detected
 		self.area=" "				# a location like "in front of the main entrace"
 		self.account=" "			# the accout the device belongs to .. something like JKW even if there are two logins (kolja,caro) to the ACCOUNT
-		self.state=0				# what state is the cam in? 0=idle
 		self.webcam=[]				# list of webcam_viewer who are watching the webcam
 		self.state=-1				# state of the cam, 0=idle, 1=alert, 2=detection disabled, idle, 3=detection disabled, movement
 		self.challange=""			# challange for the login
+		self.alert=alert_event()	# event structure
 
 # dies ist der WEBSOCKET client
 class ws_clients:
@@ -40,3 +43,14 @@ class webcam_viewer:
 		self.ws = cli 				# this shall hold the websocket client
 		self.interval = 0			# the intervall as in
 		self.ts = 0				# the timestamp of the last refresh
+
+class alert_event:
+	def __init__(self):
+		self.ts = 0				# timestamp of the first recogition of the alert
+		self.files = []				# list of filenames for that event
+		self.notification_send_ts = 0		# is the location send ? -1 means send it asap
+		self.files_expected = 5			# wait for 5 pictures till you send a mail
+		self.file_max_timeout_ms = 5000		# wait up to 5sec on pictures and send the mail if they don't appere
+		self.comm_path = 1			# +1 = via mail, +2 = via WS
+		self.email = ["kkoolljjaa@gmail.com"]	# list of all receivers
+		self.last_upload = 0
