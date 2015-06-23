81aebaa368d7efd27f8e4912ac56c01a6947c15c
Kolja RPI <kkoolljjaa@gmail.com>
2015-06-23 17:26:06 -0500

    added autostart script

--- client/run.sh
+++ client/run.sh
@@ -1,3 +1,4 @@
-#sudo uv4l --driver raspicam  --auto-video_nr --width 320 --height 240 --framerate 5
+cd /home/pi/python/illumino/client;
+uv4l --driver raspicam  --auto-video_nr --width 1280 --height 7200 --framerate 10
 export LD_PRELOAD=/usr/lib/uv4l/uv4lext/armv6l/libuv4lext.so
 python3 client.py
---
+++ client/screen.sh
@@ -0,0 +1,2 @@
+screen -S "mylittlescreen" -d -m
+screen -r "mylittlescreen" -X stuff '/home/pi/python/illumino/client/run.sh\n'
