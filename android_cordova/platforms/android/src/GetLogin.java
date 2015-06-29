f70d735c88f7950011fa9cb86b20f9e9659cb80c
kolja ubuntu <kolja.windeler@gmail.com>
2015-06-28 22:19:59 -0500

    Autologin working

--- android_cordova/platforms/android/src/GetLogin.java
+++ android_cordova/platforms/android/src/GetLogin.java
@@ -2,9 +2,16 @@ import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.provider.Settings;
 import android.widget.Toast;
+
+import com.glubsch.MainActivity;
+
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,15 +34,42 @@ public class GetLogin extends CordovaPlugin {
     }
     public boolean execute(final String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         final int duration = Toast.LENGTH_SHORT;
-// Shows a toast
-        Log.v(TAG,"CoolPlugin received:"+ action);
+        // Shows a toast
+        /*Log.v(TAG,"CoolPlugin received:"+ action);
         cordova.getActivity().runOnUiThread(new Runnable() {
             public void run() {
                 Toast toast = Toast.makeText(cordova.getActivity().getApplicationContext(), action, duration);
                 toast.show();
             }
-        });
+        });*/
+        JSONObject parameter = new JSONObject();
+        Context context = cordova.getActivity();
+        SharedPreferences mSettings = (SharedPreferences) context.getSharedPreferences(MainActivity.PREFS_NAME, context.MODE_MULTI_PROCESS);
+
+
+        if(action.equals("get")) {
+            String login = mSettings.getString("LOGIN", "Kolja");
+            String pw = mSettings.getString("PW", "hui");
+            String quality = "0";
+            if(login.equals("") || pw.equals("")) quality="-1";
+            parameter.put("q", quality);
+            parameter.put("l", login);
+            parameter.put("p", pw);
+            // callback.success(parameter);
+            PluginResult result = new PluginResult(PluginResult.Status.OK, parameter);
+            result.setKeepCallback(true);
+            callbackContext.sendPluginResult(result);
+        } else if (action.equals("set")){
+            if(!args.getString(0).equals("") && !args.getString(1).equals("")) {
+                SharedPreferences.Editor editor = mSettings.edit();
+                editor.putString("LOGIN", args.getString(0));
+                editor.putString("PW", args.getString(1));
+                editor.commit();
+            }
+        }
         return true;
-    }
+    };
+
+
 }
 
