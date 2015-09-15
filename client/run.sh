0d1cd839574fa7402bc92178549bbcfbeb9f834f
Kolja RPI <kkoolljjaa@gmail.com>
2015-09-15 17:06:04 -0500

    updated startup stuff

--- client/run.sh
+++ client/run.sh
@@ -1,4 +1,3 @@
 cd /home/pi/python/illumino/client;
-uv4l --driver raspicam  --auto-video_nr --width 1280 --height 7200 --framerate 10
 export LD_PRELOAD=/usr/lib/uv4l/uv4lext/armv6l/libuv4lext.so
 python3 client.py
