418ff398dc1e209bb0cfeffc9b418123d90bc97d
kolja ubuntu <kolja.windeler@gmail.com>
2015-10-14 22:15:56 -0500

    removed unneeded images

--- android_cordova/platforms/android/build/generated/source/r/debug/com/glubsch/R.java
+++ android_cordova/platforms/android/build/generated/source/r/debug/com/glubsch/R.java
@@ -11,9 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int logobw=0x7f020001;
-        public static final int screen=0x7f020002;
+        public static final int logobw=0x7f020000;
     }
     public static final class string {
         public static final int activity_name=0x7f040000;
