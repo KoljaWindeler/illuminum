86877f11827c0353b2837f9b891273a617cac3ca
JKW@rpi1 <KKoolljjaa@gmail.com>
2015-10-21 13:26:29 -0500

    improved start script

--- gpucam/client/prepare.sh
+++ gpucam/client/prepare.sh
@@ -1,2 +1,3 @@
 mkdir /dev/shm/mjpeg/
+killall raspimjpeg
 /home/pi/python/illumino/gpucam/raspimjpeg -v --config /home/pi/python/illumino/gpucam/raspimjpeg.config
--- gpucam/client/run.sh
+++ gpucam/client/run.sh
@@ -1,3 +1,4 @@
 cd /home/pi/python/illumino/gpucam/client;
 #export LD_PRELOAD=/usr/lib/uv4l/uv4lext/armv6l/libuv4lext.so
+sleep 30
 python3 client.py
