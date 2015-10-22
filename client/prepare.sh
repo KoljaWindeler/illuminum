95bf4d0301aef5042ad8c61af9c7015a5d104462
JKW@rpi1 <KKoolljjaa@gmail.com>
2015-10-22 09:50:39 -0500

    removed usb webcam client

--- client/prepare.sh
+++
@@ -1,1 +0,0 @@
-#uv4l --driver raspicam  --auto-video_nr --width 1280 --height 7200 --framerate 10
--- client/screen.sh
+++
@@ -1,3 +0,0 @@
-screen -S "mylittlescreen" -d -m
-screen -r "mylittlescreen" -X stuff '/home/pi/python/illumino/client/prepare.sh\n'
-screen -r "mylittlescreen" -X stuff '/home/pi/python/illumino/client/run.sh\n'
