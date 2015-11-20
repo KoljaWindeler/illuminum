c9de54a59d1a84befe922dcb08649f1fa57a5b23
KoljaWindeler <Kolja.Windeler@gmail.com>
2015-11-20 12:36:28 -0600

    added alarms array to collect alarms per MID, needed for notification

--- android_cordova/platforms/android/src/com/glubsch/s_area.java
+++ android_cordova/platforms/android/src/com/glubsch/s_area.java
@@ -13,15 +13,18 @@ class s_area {
     private Location coordinates;
     private Integer range;
     private Integer state; // debugging
+    public ArrayList<Integer> alarms = new ArrayList<Integer>();
     public ArrayList<String> clients = new ArrayList<String>();
 
-    public s_area(String name, Integer detection, Location coordinates, Integer range, Integer state, String client){
+    public s_area(String name, Integer detection, Location coordinates, Integer range, Integer state, String client, Integer alarms){
         super();
         this.name=name;                 // like "home"
         this.detection=detection;       // 0=off, 1=on, 2=fast fire
         this.coordinates=coordinates;   // a android location
         this.range = range;         // range around the center
         this.state=state;
+        this.alarms.clear();
+        this.alarms.add(alarms);
         this.clients.clear();
         this.clients.add(client);
     }
@@ -33,4 +36,29 @@ class s_area {
     public String getName()     {           return this.name;           }
     public int getState()       {           return this.state;       }
     public Location getCoordinates() {      return this.coordinates;    }
+
+    public int hasClient(String mid){
+        return this.clients.indexOf(mid);
+    };
+
+    public void addClient(String mid, int alarms){
+        this.clients.add(mid);
+        this.alarms.add(alarms);
+    }
+
+    public void setAlarms(int pos, int alarms){
+        this.alarms.set(pos, alarms);
+    }
+
+    public int getAlarms(int pos){
+        return this.alarms.get(pos);
+    }
+
+    public int getAlarmSum(){
+        int sum=0;
+        for(int i=0;i<this.alarms.size();i++) {
+            sum += this.alarms.get(i);
+        }
+        return sum;
+    }
 }
