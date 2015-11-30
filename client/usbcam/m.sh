75de56056c23b5e7c7ffa7b0a014cc1cd15cb9d6
JKW@rpi1 <KKoolljjaa@gmail.com>
2015-11-29 22:08:07 -0600

    added convert as option for usb cams

--- client/usbcam/m.sh
+++ client/usbcam/m.sh
@@ -2,3 +2,5 @@ for f in /dev/shm/mjpeg/n/*; do
  mv $f /dev/shm/mjpeg/cam_full.jpg;
 done
 cp /dev/shm/mjpeg/cam_full.jpg /dev/shm/mjpeg/cam_prev.jpg;
+#install imagemagick for the convert, will cost even more cpu but less bandwidth
+#convert -resize 640x480 /dev/shm/mjpeg/cam_full.jpg /dev/shm/mjpeg/cam_prev.jpg;
