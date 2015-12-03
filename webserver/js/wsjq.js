1b58e57c623b8297671b27dcb8b46fd34caeeaf6
Kolja EC2 <KOlja.Windeler@gmail.com>
2015-12-03 14:40:45 -0600

    moved to domain www.illuminum.de *proud*

--- webserver/js/wsjq.js
+++ webserver/js/wsjq.js
@@ -1,7 +1,8 @@
 // connection
 var con = null;
 //var IP="52.24.157.229";
-var IP="illuminum.speedoino.de";
+//var IP="illuminum.speedoino.de";
+var IP="illuminum.de";
 var host="https://"+IP+"/";
 var prelogin="";
 var fast_reconnect=0;
