a544d29e58571e018d77c33d1dcf82e308331f38
KoljaWindeler <Kolja.Windeler@gmail.com>
2015-12-03 14:57:51 -0600

    moved to own domain

--- android_cordova/platforms/android/src/com/glubsch/s_ws.java
+++ android_cordova/platforms/android/src/com/glubsch/s_ws.java
@@ -66,7 +66,7 @@ public class s_ws implements WebSocketConnectionObserver {
 
     public void createWebSocket() {
 
-        final String wsuri = "wss://illuminum.speedoino.de:9879";
+        final String wsuri = "wss://illuminum.de:9879";
 
         try {
 
