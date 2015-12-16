5e041427bf6c4068e988e3f76780fab34df1420a
JKW@rpi1 <KKoolljjaa@gmail.com>
2015-12-16 11:13:15 -0600

    updated update script to remount

--- client/update.sh
+++ client/update.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
+mount -o remount,rw / > /dev/null &2>1; #will fail on all "non-kolja" raspberrys .. but thats ok
+
 pushd `dirname $0` > /dev/null
 DIR=`pwd -P`
 popd > /dev/null
 
-cd python
-rw > dev/null &2>1; #will fail on all "non-kolja" raspberrys .. but thats ok
-echo "====================================================="
-echo "=============== 1. Updating ========================="
-echo "====================================================="
-git pull
-
-echo "====================================================="
-echo "=============== 2. Rebooting ========================"
-echo "====================================================="
-reboot
+cd $DIR/python
+sudo -u pi git pull
