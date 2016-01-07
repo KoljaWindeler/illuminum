29c9e539bbf03a9e304d3304b63aa2b289bd748f
JKW@rpi1 <KKoolljjaa@gmail.com>
2016-01-06 20:44:37 -0600

    reduced quali

--- client/usbcam/start_usbcam.sh
+++ client/usbcam/start_usbcam.sh
@@ -3,4 +3,6 @@
 mkdir /dev/shm/mjpeg > /dev/null 2>&1;
 mkdir /dev/shm/mjpeg/n > /dev/null 2>&1;
 
-./mjpg_streamer -i "./input_uvc.so -f 4 -r 1280x720 -n -y -quality 75"  -o "./output_file.so -f /dev/shm/mjpeg/n -c ./m.sh"&
+./mjpg_streamer -i "./input_uvc.so -f 4 -r 1280x720 -n  -quality 60"  -o "./output_file.so -f /dev/shm/mjpeg/n -c ./m.sh"&
+#./mjpg_streamer -i "./input_uvc.so -f 4 -r 1280x720 -n  -quality 75"  -o "./output_file.so -f /dev/shm/mjpeg/n -c ./m.sh"&
+#./mjpg_streamer -i "./input_uvc.so -f 4 -r 640x480 -n  -quality 75"  -o "./output_file.so -f /dev/shm/mjpeg/n -c ./m.sh"&
