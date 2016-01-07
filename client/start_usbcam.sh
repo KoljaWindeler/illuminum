70c87de1d931ec5b78df957eb3b6aad80922d251
Kolja.Windeler@C.H.I.P <Kolja.Windeler@gmail.com>
2016-01-07 04:42:17 +0000

    moved start usb to main level

--- client/start_usbcam.sh
+++ client/start_usbcam.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+locationOfScript=$(dirname "$(readlink -e "$0")")"/usbcam"
+
+killall mjpg_streamer  > /dev/null 2>&1;
 
 mkdir /dev/shm/mjpeg > /dev/null 2>&1;
 mkdir /dev/shm/mjpeg/n > /dev/null 2>&1;
 
-./mjpg_streamer -i "./input_uvc.so -f 4 -r 1280x720 -n  -quality 60"  -o "./output_file.so -f /dev/shm/mjpeg/n -c ./m.sh"&
+$locationOfScript/mjpg_streamer -i $locationOfScript"/input_uvc.so -f 4 -r 1280x720 -n  -quality 60"  -o $locationOfScript"/output_file.so -f /dev/shm/mjpeg/n -c $locationOfScript/m.sh"&
 #./mjpg_streamer -i "./input_uvc.so -f 4 -r 1280x720 -n  -quality 75"  -o "./output_file.so -f /dev/shm/mjpeg/n -c ./m.sh"&
 #./mjpg_streamer -i "./input_uvc.so -f 4 -r 640x480 -n  -quality 75"  -o "./output_file.so -f /dev/shm/mjpeg/n -c ./m.sh"&
