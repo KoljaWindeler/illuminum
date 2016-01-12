0433cc32575e16fbbec8e892907dd738af539119
Kolja.Windeler@C.H.I.P <Kolja.Windeler@gmail.com>
2016-01-12 11:07:03 -0600

    added fsys GPIO support

--- client/python/u_gpio.py
+++ client/python/u_gpio.py
@@ -10,15 +10,20 @@ try:
 except:
 	rpi_support = 0
 
-# import fsys if posible
-try:
-	import asdf as GPIO 
-except:
+# import fsys for CHIP if posible
+# XIO-P0   408    13
+# XIO-P1   409    14
+# XIO-P2   410    15
+# XIO-P3   411    16
+# XIO-P4   412    17
+# XIO-P5   413    18
+# XIO-P6   414    19
+# XIO-P7   415    20
+if(rpi_support):
 	fsys_support = 0
 
 
 class u_gpio:
-
 	def __init__(self):
 		self.PIN_PIR 		= 1
 		self.PIN_MOVEMENT 	= 2
@@ -45,19 +50,73 @@ class u_gpio:
 			GPIO.setup(self.PIN_USER, 		GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 			GPIO.setup(self.PIN_CAM, 		GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+		elif(fsys_support):
+			p.rint("configuring fsys pins","g")
+
+			self.PIN_PIR 		= 408
+			self.PIN_MOVEMENT 	= 409	
+			self.PIN_DETECTION 	= 410
+			self.PIN_USER 		= 411
+			self.PIN_CAM 		= 412
+
+			self.fsys_export(self.PIN_PIR)
+			self.fsys_export(self.PIN_MOVEMENT)
+			self.fsys_export(self.PIN_DETECTION)
+			self.fsys_export(self.PIN_USER)
+			self.fsys_export(self.PIN_CAM)
+
+
 	def set(self,pin,level):
-		if(level==0):
-			if(rpi_support):
+		if(rpi_support):
+			if(level==0):
 				p.rint("setting pin "+str(pin)+" weak low","g")
 				GPIO.setup(pin,	GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-		
-		else:
-			if(rpi_support):
+			else:
 				p.rint("setting pin "+str(pin)+" high","g")
 				GPIO.setup(pin, GPIO.OUT)
 				GPIO.output(pin,1)
+		elif(fsys_support and pin>=408 and pin<=415):
+			try:
+				gpiopin = "gpio%s" % (str(pin))
+				f = open("/sys/class/gpio/"+gpiopin+"/direction","w")
+				f.write("out")
+				f.close()
+				f = open("/sys/class/gpio/"+gpiopin+"/value","w")
+				if(level==0):
+					f.write("0")
+				else:
+					f.write("1")
+				f.close()
+			except:
+				p.warn("failed to write to pin "+str(pin))
+
 
 	def get(self,pin):
 		if(rpi_support):
-			return GPIO.input(pin)
-		return 0
+			return int(GPIO.input(pin))
+		elif(fsys_support and pin>=408 and pin<=415):
+			try:
+				gpiopin = "gpio%s" % (str(pin))
+				f = open("/sys/class/gpio/"+gpiopin+"/direction","w")
+				f.write("in")
+				f.close()
+				f = open("/sys/class/gpio/"+gpiopin+"/value","r")
+				val=f.read()
+				f.close()
+				#rint("read from pin "+str(pin)+" returned "+str(int(val)))
+				return int(val)
+			except:
+				p.warn("failed to read from pin "+str(pin))
+		return int(0)
+
+	def fsys_export(self,pin):
+		if(pin<408 or pin>415):
+			p.warn("fsys export error, pin "+str(pin)+" out of range (408-415)")
+		else:
+			try:
+				f = open("/sys/class/gpio/export","w")
+				f.write(str(pin))
+				f.close()
+			except:
+				p.warn("Failed to set pin "+str(pin)+" as output")
+
