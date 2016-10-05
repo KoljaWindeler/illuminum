c7afd257fbca6a1d0e5ca1388397b7c8220573b7
KoljaWindeler <Kolja.Windeler@gmail.com>
2016-10-05 15:31:39 +0200

    added classes for the watcher function

---
+++ client/python/classes.py
@@ -0,0 +1,11 @@
+class watcher:
+	def __init__(self,state):
+		self.clients = [] 				# list of all watcher_m2m clients
+		self.handle_movements_stopped = ""			# the handle for the 'after'-functions
+		self.handle_movements_started = ""			# the handle for the 'after'-functions
+		self.output = 0		# state of the output
+		
+class watcher_m2m:
+	def __init__(self,mid,state):
+		self.mid = mid 				# the mid of the m2m
+		self.state = state			# the state of the m2m
